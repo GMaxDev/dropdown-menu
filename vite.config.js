86a51041b824a0f77394a5c86e635ecd857c1fea
GMaxDev <maxime.guigra@gmail.com>
2024-09-25 10:44:09 +0200

    feat: package update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: './src/DropdownMenu.jsx', // Point d'entrée
+      name: 'DropdownMenu', // Nom du component
+      fileName: (format) => `dropdown-menu.${format}.js`, // Le nom de sortie pour chaque format
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'], // Exclure React du bundle pour éviter les doublons
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
+  plugins: [react()]
 })
